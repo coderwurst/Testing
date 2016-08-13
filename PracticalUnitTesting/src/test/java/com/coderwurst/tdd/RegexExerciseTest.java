c013d5cee233799fdd7e3841fef1eb149854bfb2
coderwurst <coderwurst@gmail.com>
2016-08-13 09:10:51 +0200

    Regex refactor tests

--- PracticalUnitTesting/src/test/java/com/coderwurst/tdd/RegexExerciseTest.java
+++ PracticalUnitTesting/src/test/java/com/coderwurst/tdd/RegexExerciseTest.java
@@ -5,10 +5,31 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
+@RunWith(JUnitParamsRunner.class)
 public class RegexExerciseTest {
 
 	RegexExercise regex = new RegexExercise();
+	
+	public Object [] validTestStrings() {
+		return new Object [] {
+				new Object [] {"123"},
+				new Object [] {"somewhere154digit"},
+				new Object [] {"morethan123456789"}
+		};
+	}
+	
+	public Object [] invalidTestStrings() {
+		return new Object [] {
+				new Object [] {"blah blah"},
+				new Object [] {"noDigitsHere"},
+				new Object [] {"one two three"}
+		};
+	}
 	
 	@Test
 	public void testReturnsTrueWhenStringContainsDigits() {
@@ -25,15 +46,17 @@ public class RegexExerciseTest {
 	}
 	
 	@Test
-	public void testReturnsTrueForStringWithThreeDigits() {
-		boolean result = regex.containsThreeDigits("abc123");
+	@Parameters(method = "validTestStrings")
+	public void testReturnsTrueForStringWithThreeDigits(String test) {
+		boolean result = regex.containsThreeDigits(test);
 		
 		assertTrue(result);
 	}
 	
 	@Test
-	public void testReturnsFalseForStringWithLessThanThreeDigits() {
-		boolean result = regex.containsThreeDigits("abc12");
+	@Parameters(method = "invalidTestStrings")
+	public void testReturnsFalseForStringWithLessThanThreeDigits(String test) {
+		boolean result = regex.containsThreeDigits(test);
 		
 		assertFalse(result);
 	}
